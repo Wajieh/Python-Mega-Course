c13d00f6e50380fbe2be8f6f4be821a02a6227a6
Wajieh <wajiehbadr@gmail.com>
2025-07-18 12:17:06 +0500

    App 7 complete

--- Python-Mega-Course/App-7/backend.py
+++ Python-Mega-Course/App-7/backend.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "141710af2113bab9f55ef73e1bcd33d5"
 
 
-def get_data(place, forecast_days=None):
+def get_data(place, forecast_days=None, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
@@ -12,6 +12,5 @@ def get_data(place, forecast_days=None):
     filtered_data = filtered_data[:nr_values]
     return filtered_data
 
-
 if __name__=="__main__":
-    print(get_data(place="Tokyo", forecast_days=3))
+    print(get_data(place="Tokyo", forecast_days = 3))
