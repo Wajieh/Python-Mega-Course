ef98cefd39f6b9f813a7cf5a7bfe137ea8354569
Wajieh <wajiehbadr@gmail.com>
2025-07-29 17:57:27 +0500

    App 15 complete

--- Python-Mega-Course/App-15/main.py
+++ Python-Mega-Course/App-15/main.py
@@ -4,9 +4,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import os
 
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
+
+prefs ={'download.default_directory' : os.getcwd()} # Current Working Directory
+chrome_options.add_experimental_option('prefs',prefs)
+
 service = Service("chromedriver-win64")
 driver = webdriver.Chrome(options=chrome_options,service=service)
 driver.get("https://demoqa.com/login")
@@ -41,5 +46,11 @@ email_field.send_keys('name@name.com')
 currenaddress_field.send_keys('Australia')
 permanentaddress_field.send_keys('New Zealand')
 driver.execute_script('arguments[0].click();',submit_button)
+
+#Download 
+uplaod_download = (WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.ID,'item-7'))))
+uplaod_download.click()
+download_button = driver.find_element(By.ID,'downloadButton')
+driver.execute_script('arguments[0].click();',download_button)
 
 driver.quit()
