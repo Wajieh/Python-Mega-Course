5ef5e1943c90e3f7a2c748d549379cfe0c6f73be
Wajieh <wajiehbadr@gmail.com>
2025-08-01 10:49:42 +0500

    Part 3 V2/V3

--- Python-Mega-Course/App-17/job_application/admin.py
+++ Python-Mega-Course/App-17/job_application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Form
 # Register your models here.
+
+
+class FormAdmin(admin.ModelAdmin):
+    
+    list_display = ("firstname", "lastname", "email")
+    search_fields = ("firstname", "lastname", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("firstname", )
+    readonly_fields = ("occupation",)
+admin.site.register(Form,FormAdmin)
+
